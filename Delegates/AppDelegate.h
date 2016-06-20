054a3bfcd88b28a79f7ac204b13b1b661003f932
tolyasjar <toleenj@gmail.com>
2016-06-20 12:12:03 -0500

    Initial Commit

---
+++ Delegates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Delegates
+//
+//  Created by Toleen Jaradat on 6/20/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Delegates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Delegates
+//
+//  Created by Toleen Jaradat on 6/20/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
