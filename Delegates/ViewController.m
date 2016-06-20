61841e46991b0c74bc25cabf88f14fac3f84e973
tolyasjar <toleenjaradat@Toleens-MacBook-Pro.local>
2016-06-20 13:56:23 -0500

    Delegates

--- Delegates/ViewController.m
+++ Delegates/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (nonatomic,weak) IBOutlet UISwitch *beginEditingSwitch;
+@property (nonatomic,weak) IBOutlet UISwitch *endEditingSwitch;
+@property (nonatomic,weak) IBOutlet UISwitch *changeCharactersInRange;
+@property (nonatomic,weak) IBOutlet UISwitch *clearSwitch;
+@property (nonatomic,weak) IBOutlet UISwitch *returnSwitch;
+
 @end
 
 @implementation ViewController
@@ -22,6 +28,49 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    
+    if ([_beginEditingSwitch isOn])
+    { NSLog(@"textFieldShouldBeginEditing"); }
+    
+    return YES;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    
+    if ([_endEditingSwitch isOn])
+    { NSLog(@"textFieldShouldEndEditing"); }
+    
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    
+    if ([_changeCharactersInRange isOn])
+    { NSLog(@"textFieldshouldChangeCharactersInRange"); }
+    
+    return YES;
+}
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField {
+    
+    if ([_clearSwitch isOn])
+    { NSLog(@"textFieldShouldClear");
+        return YES;}
+    else return NO;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    
+    if ([_returnSwitch isOn])
+    { NSLog(@"textFieldShouldReturn");
+    [textField resignFirstResponder];
+    }
+    
+    return YES;
 }
 
 @end
